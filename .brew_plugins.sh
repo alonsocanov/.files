4117b938927a7a276961b884f1225e39089cf3bb
Alonso Cano <45244628+alonsocanov@users.noreply.github.com>
2025-12-18 13:03:57 -0600

    removed: autosugestions from brew

--- .brew_plugins.sh
+++ .brew_plugins.sh
@@ -1,18 +1,18 @@
 if [[ -d $(brew --prefix) ]]; then
-    #syntax higliting
-    if [[ -f $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
-        source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    # #syntax higliting
+    # if [[ -f $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+    #     source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-    else
-        echo "Syntax hylighting not installed"
-    fi
+    # else
+    #     echo "Syntax hylighting not installed"
+    # fi
 
     # autosugestions
-    if [[ -f $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
-        source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-    else
-        echo "Autosuggestions not installed"
-    fi
+    # if [[ -f $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+    #     source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    # else
+    #     echo "Autosuggestions not installed"
+    # fi
 
     # # completions
     # if [[ -f $(brew --prefix)/share/zsh-completions/zsh-completions.zsh ]]; then
